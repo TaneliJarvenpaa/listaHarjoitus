f5913b11e5232483f2d7059329769f6b02999945
TaneliJarvenpaa <114722920+TaneliJarvenpaa@users.noreply.github.com>
2023-08-27 13:00:03 +0300

    tehty tehtävät

---
+++ src/main/java/com/example/listaHarjoitus/ListaHarjoitusApplication.java
@@ -0,0 +1,13 @@
+package com.example.listaHarjoitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaHarjoitusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaHarjoitusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/listaHarjoitus/ListaHarjoitusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listaHarjoitus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaHarjoitusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
